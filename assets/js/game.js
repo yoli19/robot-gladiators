6fd7677a1139c414d7ce72c31e3ac5d811ed698c
Yolanda Jacobs <y.jacobs@hotmail.com>
2022-04-03 15:49:57 -0400

    multiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -68,7 +68,13 @@ var fight = function(enemyName) {
 }; // end of fight function
 
 for(var i = 0; i < enemyNames.length; i++) {
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    fight(enemyNames[i]);
+    if (playerHealth > 0) {
+        window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+        var pickedEnemyName = enemyNames[i];
+        enemyHealth = 50;
+        fight(enemyNames[i]);
+    } else {
+        window.alert("You have lost your robot in battle! Game Over!");
+        break;
+    }
 }
